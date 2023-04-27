25bbf7303d2c37fc2c70990bca92fec9e2faf4e9
jonasv <jonas.vlietinck@aeronomie.be>
2023-04-27 23:11:21 +0200

    different way of creating harp compliant output and change in output variables.
    Harp commands, like export_product and harp.Variable are used now.
    only qdoas variables for which a mapping is known are written to output.
    change in cml interface: adding option to select which slant columns are of interest.

--- qdoas2harp/convert2harp.py
+++ qdoas2harp/convert2harp.py
@@ -1,24 +1,19 @@
 #!/space/hpc-apps/bira/19i/py36/bin/python3
-import IPython
 import harp #module load 19g/py37
 import harp._harppy as harppy
 import numpy as np
-import numpy.ma as ma
 import netCDF4
 import datetime as dt
 import glob
 import os,sys
 import subprocess
 import re
-from dateutil.rrule import rrule, DAILY
-import collections
-import shutil
 from datetime import date
 import time
 import argparse
 from . import nctools as nct
 from .qd2hp_mapping import qd2hp_mapping
-
+import IPython
 #calib group is excluded. 
 
 # qd_dim={'scanlines':'n_alongtrack','rows':'n_crosstrack'}
@@ -34,26 +29,39 @@ class Dataset(netCDF4.Dataset): #https://github.com/Unidata/netcdf4-python/issue
 
 def cml():
     '''entry_point used as a cml argument for input qdoas filename and will generate an output filename '''
-    helpstr='qdoas output file'
+    helpstr='qdoas output file' 
     parser = argparse.ArgumentParser(description='give L1 file')
     # parser.add_argument('-file', dest='file', action='store', required=True,help=helpstr, default=None)
     parser.add_argument('-qdoas_inp',dest='qdoasfile',help="qdoas file to be converted to harp compliant file")
     parser.add_argument('-jsonfile',dest='jsonfile',help="json file with the mapping and attributes for the harp variables")
     parser.add_argument('-outdir',dest='outdir',help="harp compliant qdoas output files per fitting window")
+    parser.add_argument('-slcol', default=[], nargs='+')
+    # IPython.embed();exit()
     args=parser.parse_args()
     qdoasfile=args.qdoasfile
     jsonfile=args.jsonfile
     outdir=args.outdir
+    slcol=args.slcol
+    # IPython.embed();exit()
+    slcol_dict={}
+    for x in slcol:
+        assert re.search("\s*(\S*)\s*=\s*(\S*)\s*",slcol[0])!=None, "check input of -slcol option"
+        slcol_dict[re.search("\s*(\S*)\s*=\s*(\S*)\s*",x).group(1)]=re.search("\s*(\S*)\s*=\s*(\S*)\s*",x).group(2)
     assert os.path.isdir(outdir)
+    # IPython.embed();exit()
+
     if os.path.isdir(qdoasfile):
+        #process all files in the given directory
         for fileqd in glob.glob(qdoasfile+"/*"):
-            makeharp(fileqd,jsonfile,outdir)
+            assert fileqd[-2:]=="nc"
+            makeharp(fileqd,outdir,slcol_dict)
     else:
-        makeharp(qdoasfile,jsonfile,outdir)
+        assert qdoasfile[-2:]=="nc"
+        makeharp(qdoasfile,outdir,slcol_dict)
 
     
 
-def makeharp(qdfile,jsonfile,outdir):
+def makeharp(qdfile,outdir,slcol_dict):
     groups=[ x for x in  list(set(nct.listallgroups(qdfile))) ]
     # group name Calib is fixed in qdoas, see engine/output_netcdf.cpp:31:const static string calib_subgroup_name = "Calib";
     maingroups=[x.split('/')[1] for x in groups]
@@ -64,110 +72,90 @@ def makeharp(qdfile,jsonfile,outdir):
     assert np.all([maingroup==x.split('/')[1] for x in groups])
     assert np.all(['Calib'==x for x in calibgroups]) or len(calibgroups==0)
     for fitwin in subgroups:
-        qdoas_meta=get_qdoasmeta(qdfile,maingroup)
         harpout=outdir+re.sub(r'.*/([^.]+)(.*)',r'\1_{}\2'.format(fitwin),qdfile)
-        # IPython.embed();exit()
-        with Dataset(qdfile,'r') as ncqdoas, Dataset(harpout,'w') as ncharp:
+        with Dataset(qdfile,'r') as ncqdoas:
             fitgr="/"+maingroup+"/"+fitwin+"/"
             maingr="/"+maingroup+"/"
             mainvars=[maingr+x for x in  ncqdoas[maingr].variables.keys()]
             fitvars=[fitgr+x for x in  ncqdoas[fitgr].variables.keys()]
-            time_scans=qdoas_meta['scanlines']*qdoas_meta['rows']
-            time=ncharp.createDimension("time",time_scans)
-            ncharp.Conventions='HARP-1.0'
-            ncharp.L1_InputFile=ncqdoas[maingr].InputFile.split('/')[-1]
+            # time=ncharp.createDimension("time",time_scans)
+            # ncharp.Conventions='HARP-1.0'
+            
+           
+            dd=qd2hp_mapping(mainvars+fitvars,slcol_dict)
+            
+
+            create_ncharpvar(dd,ncqdoas,harpout)
             # IPython.embed();exit()
+            export_metadata(ncqdoas,maingr, harpout)
 
-            if '4' in ncqdoas[maingr].dimensions.keys(): #this corresponds to the independent_4 dim in harp for the ground pixel corners:
-                corners=ncharp.createDimension("independent_4",4)
-            dd=qd2hp_mapping(jsonfile,mainvars+fitvars)
-            create_ncharpvar(dd,ncqdoas,ncharp)
-
+            
+def export_metadata(ncqdoas,maingr,harpout):
+    #export some main attributes that cannot be added with a harp export command. 
+    with nct.Dataset(harpout,'a') as ncharp:
+        ncharp.L1_InputFile=ncqdoas[maingr].InputFile.split('/')[-1]
 
 
     
         
-def create_ncharpvar(dd,ncqdoas,ncharp):
+def create_ncharpvar(dd,ncqdoas,output_filename):
+    product = harp.Product()
     for qdvar in dd.keys():
         hpobj=dd[qdvar]
-        print(hpobj)
+        hpvar=harp.Variable(hpobj.harpname)
         assert ncqdoas[qdvar].dtype.char in ['f','d','h','s','b','B','c','i','l','H','I']
         if ncqdoas[qdvar].dtype.char in ['f','d']:#needs conversion of fillvalues to nan
-            # IPython.embed();exit()
             if ncqdoas[qdvar].dimensions==('n_alongtrack', 'n_crosstrack'):
-                nchpvar=ncharp.createVariable(hpobj.harpname,'f4', ("time"),fill_value=False)
-                nanvar=nct.makemasked(ncqdoas[qdvar][:].flatten())
+                dimensions=["time"]
+                nanvar=nct.makemasked(ncqdoas[qdvar][:].flatten()).astype('f',casting='same_kind')
             elif ncqdoas[qdvar].dimensions==('n_alongtrack', 'n_crosstrack','4'):
-                nchpvar=ncharp.createVariable(hpobj.harpname,'f4',("time","independent_4"),fill_value=False)
-                nanvar=nct.makemasked(ncqdoas[qdvar][:].reshape(-1,4))
-            if hpobj.comment!=None: nchpvar.comment=hpobj.comment
-            if hpobj.descrp!=None: nchpvar.description=hpobj.descrp 
-            if hpobj.units!=None: nchpvar.units=hpobj.units
-            if hpobj.valid_min!=None: nchpvar.valid_min=hpobj.valid_min 
-            if hpobj.valid_max!=None: nchpvar.valid_max=hpobj.valid_max 
-
-            nchpvar[:]=nanvar
+                dimensions=["time",None]
+                nanvar=nct.makemasked(ncqdoas[qdvar][:].reshape(-1,4)).astype('f',casting='same_kind')
+            hpvar=harp.Variable(nanvar,dimensions, unit=hpobj.units, valid_min=hpobj.valid_min, valid_max=hpobj.valid_max, description=hpobj.description , enum=None)
         else: #non floating point, integer point:
             if ncqdoas[qdvar].dimensions==('n_alongtrack','n_crosstrack'):
                 # continue
                 datatype=ncqdoas[qdvar].dtype.char
                 if ncqdoas[qdvar].dtype.char=='H':
                     datatype='h'
-                nchpvar=ncharp.createVariable(hpobj.harpname,datatype, ("time"),fill_value=False)
                 nchpvar[:]=ncqdoas[qdvar][:].flatten()
-                if hpobj.comment!=None: nchpvar.comment=hpobj.comment
-                if hpobj.descrp!=None: nchpvar.description=hpobj.descrp 
-                if hpobj.units!=None: nchpvar.units=hpobj.units
-                if hpobj.valid_min!=None: nchpvar.valid_min=hpobj.valid_min 
-                if hpobj.valid_max!=None: nchpvar.valid_max=hpobj.valid_max 
+                hpvar=harp.Variable(nchpvar,['time'], unit=hpobj.units, valid_min=hpobj.valid_min, valid_max=hpobj.valid_max, description=hpobj.description, enum=None)
 
             elif hpobj.harpname=="datetime_start": #exception for times. 
                 assert ncqdoas[qdvar].dimensions==('n_alongtrack', 'n_crosstrack','datetime')
-                nchpvar=ncharp.createVariable(hpobj.harpname,'f8', ("time"),fill_value=False)
-
                 tt=nct.makemasked(ncqdoas[qdvar][:])
-                
                 #bug in qdoas that some times are fillvalues for some rows>0, temporary solution is to take the times from rows=0, since times for all rows is the same. 
                 idxnan=np.nonzero(np.isnan(tt))
                 if idxnan:
                     idxnan2=(idxnan[0],np.zeros((len(idxnan[1]),),dtype=np.int32),idxnan[2])
                     tt[idxnan]=tt[idxnan2]
                 tt=tt.reshape(-1,ncqdoas[qdvar].shape[-1])
-                arr=np.full((tt.shape[0],),None)
+                #end of bug fix from qdoas
+                arr=np.full((tt.shape[0],),np.nan,dtype='d')
                 for j in range(0,tt.shape[0]):
                     bb=[ int(tt[j,i]) for i in range(0,7)]
                     #WARNING ref. time should be taken from the units attr.
-                    # IPython.embed();exit()
                     reftimestr=re.search("\d{4}-\d{2}-\d{2}",hpobj.units).group(0)
                     assert reftimestr!=None, "check reference time format"
                     reftime=dt.datetime.strptime(reftimestr,"%Y-%m-%d")
                     arr[j]=(dt.datetime(*bb)-reftime).total_seconds()
-                    assert arr[j]>0, "reference time too late"
-                    if hpobj.comment!=None: nchpvar.comment=hpobj.comment
-                    if hpobj.descrp!=None: nchpvar.description=hpobj.descrp 
-                    if hpobj.units!=None: nchpvar.units=hpobj.units
-                    
-                nchpvar[:]=arr
-            else:
-                    assert 0, "unknown variable for this conversion"
-            
+                    # IPython.embed();exit()
 
+                    assert arr[j]>0, "reference time too late"
+                arr=arr.astype('d',casting='same_kind')
+                hpvar=harp.Variable(arr,['time'], unit=hpobj.units, valid_min=hpobj.valid_min, valid_max=hpobj.valid_max, description=hpobj.description, enum=None)
+            else:
+                assert 0, "unknown variable for this  conversion"
+            
+        obj=setattr(product,dd[qdvar].harpname,hpvar) #add harp variables as attribute to harp product.
+    # IPython.embed();exit()
+    print(")))))))))))")
+    harp.export_product(product,output_filename , file_format='hdf5', hdf5_compression=6)
+        # IPython.embed();exit()
+    
     
             
-            
-def get_qdoasmeta(qdoas_l2,maingroup):
-    qdoas_meta={}
-    maingroup="/"+maingroup+"/"
-    with Dataset(qdoas_l2,'r') as ncqdoas:
-        inputfile=ncqdoas[maingroup].InputFile.split('/')[-1]
-        qdoas_meta['scanlines']=ncqdoas[maingroup+'Latitude'][:].shape[0]
-        qdoas_meta['rows']=ncqdoas[maingroup+'Latitude'][:].shape[1]
-        # assert np.all(nct.makemasked(ncqdoas[maingroup+'/Orbit number'][:])==nct.makemasked(ncqdoas[maingroup+'/Orbit number'][0,0])), qdoas_l2
-        # qdoas_meta['orbit']=int(nct.makemasked(ncqdoas[maingroup+'/Orbit number'][0,0]))
-    return qdoas_meta
-
-
-
+     
 
 
 
--- qdoas2harp/qd2hp_mapping.py
+++ qdoas2harp/qd2hp_mapping.py
@@ -14,35 +14,46 @@ from attr import define,  Factory
 import json
 
 
-exclude_qdoasvar=['Calib/','Spec No','Spike removal','Orbit number','Stretch(Spectrum)','Err Stretch(Spectrum)','Day number','Year','Fractional time' ]
+mapping_file="qd2hp_mapping.json" #contains a mapping of default qdoas names, like geolocation, time. 
+molecule_fullname={'HCHO': 'formaldehyde',
+                   'SO2': 'sulfur dioxide',
+                   'BrO': 'Bromine oxide',
+                   'NO2': 'nitrogen dioxide',
+                   'C2H2O2': 'glyoxal'} 
 
 
-def qd2hp_mapping(jsonfile, qd_vars):
+def qd2hp_mapping(qd_vars,slcol_dict):
     '''qd_vars  contains variables names used in qdoas without the groups prepended.'''
 
-    dd={}
-    mapping=readconfig(jsonfile)
+    qd2hp_dict={}
+    assert np.all([slcol_dict[x] in molecule_fullname.keys() for x in slcol_dict.keys()]),"wrong molecule name"
+    mapping=readconfig(mapping_file)
     for qdvar_path in qd_vars:
         qdvar=qdvar_path.split('/')[-1]
-        if qdvar  in exclude_qdoasvar:
-            continue
-        if qdvar in mapping.keys():
-            dd[qdvar_path]=mapping[qdvar]
-        else:
-            tempvarharp=re.sub(r"[\(\)](?!$)","_",qdvar)
-            tempvarharp=re.sub(r"[\s\-]+(?!$)","_",tempvarharp)
-            harpname=re.sub(r"\)$","",tempvarharp)
-            dd[qdvar_path]=qd2hp_entry(harpname)
-        
-    return dd
+        #search if qdoas variable is a  variable name for which a standard mapping exist.  
+        if qdvar in mapping: 
+            qd2hp_dict[qdvar_path]=mapping[qdvar]
+        #search in the slant columns is from a molecule of interest
+        elif re.search("SlCol\((.*)\)",qdvar)!=None:
+            qdoas_name=re.search("SlCol\((.*)\)",qdvar).group(1)
+            if qdoas_name in slcol_dict.keys():
+                qd2hp_dict[qdvar_path]=qd2hp_entry.create_from_slcol(slcol_dict[qdoas_name])
+        #search in the slant columns error is from a molecule  of interest
+        elif re.search("SlErr\((.*)\)",qdvar)!=None:
+            molecule=re.search("SlErr\((.*)\)",qdvar).group(1)
+            if qdoas_name in slcol_dict.keys():
+                qd2hp_dict[qdvar_path]=qd2hp_entry.create_from_slerr(slcol_dict[qdoas_name])
+    
+
+    return qd2hp_dict
 
 
 
 @define(repr=False)
 class qd2hp_entry:
-    harpname=field()
+    harpname=field() 
     units: str = field(kw_only=True,default=None)
-    descrp: str = field(kw_only=True,default=None)
+    description: str = field(kw_only=True,default=None)
     comment: str = field(kw_only=True,default=None)
     valid_min: float = field(kw_only=True,default=None)
     valid_max: float = field(kw_only=True,default=None)
@@ -50,9 +61,22 @@ class qd2hp_entry:
     def __repr__(self):
         return self.harpname
 
+    @classmethod
+    def create_from_slcol(cls,molecule):
+        harpname="{}_slant_column_number_density".format(molecule)
+        units= "molecules/cm2"
+        description="slant column {}".format(molecule_fullname[molecule])
+        return cls(harpname=harpname,units=units,description=description)
+
+    @classmethod
+    def create_from_slerr(cls,molecule):
+        harpname="{}_slant_column_number_density_uncertainty".format(molecule)
+        units= "molecules/cm2"
+        description="uncertainty slant column {}".format(molecule_fullname[molecule])
+        return cls(harpname=harpname,units=units,description=description)
 
 
-    
+
 
 
 def readconfig(jsonfile):
@@ -61,4 +85,10 @@ def readconfig(jsonfile):
     for k in data.keys():
         data[k]=structure(data[k],qd2hp_entry)
     return data
+      
+
+
+    
+
+    
       
